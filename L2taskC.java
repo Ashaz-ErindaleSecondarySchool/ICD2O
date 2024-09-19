fc77b8aaf8c9faab5c02e1279202741dbaaed7fb
TerribleProgrammer124 <783827@pdsb.net>
2024-09-19 12:45:57 -0400

    Create L2taskC.java

---
+++ L2taskC.java
@@ -0,0 +1,10 @@
+class L2taskC
+{
+  public static void main(String [] args) 
+  {
+    double circ;
+    double rad = 3.0;
+    circ = 2 * Math.PI * rad; 
+    System.out.println("The circumference with a radius of 3 is " + circ + ".");
+  }
+}
