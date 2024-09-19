f45aca13ec0aa7fe83fcfdae52e74ba1565c5841
TerribleProgrammer124 <783827@pdsb.net>
2024-09-19 13:05:42 -0400

    Create L3taskA.java

---
+++ L3taskA.java
@@ -0,0 +1,12 @@
+class L3taskA
+{
+  public static void main(String [] args) 
+  {
+    /*I think it will output:
+    Welcome to Sanjay's Koala Party!
+    It's possible Sanjay can distribute 12 cookies for each bowl.*/
+    System.out.println("Welcome to Sanjay's Koala Party!");
+    System.out.print("It's possible Sanjay can distribute " + 12);
+    System.out.println(" cookies for each bowl.");
+  }
+}
