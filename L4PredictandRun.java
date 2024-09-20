16234c2678b6ec8701b45576f8759662274f3392
TerribleProgrammer124 <783827@pdsb.net>
2024-09-19 21:59:51 -0400

    Create L4PredictandRun.java

---
+++ L4PredictandRun.java
@@ -0,0 +1,9 @@
+class L4PredictandRun
+{
+    public static void main(String[] args) 
+    {
+      /*I think it will output:24
+      When I ran it the output actually was:24*/
+      System.out.println(24);
+    }
+}
