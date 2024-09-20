a5ef84c87978414722ed72ae58c473fc2ec48d64
TerribleProgrammer124 <783827@pdsb.net>
2024-09-19 22:18:20 -0400

    Create L4Modify2.java

---
+++ L4Modify2.java
@@ -0,0 +1,9 @@
+class L4Modify2
+{
+    public static void main(String[] args) 
+    {
+      int amount = 7*6;
+      System.out.println(amount);
+      System.out.println("Forty-two is: " + amount);
+    }
+}
