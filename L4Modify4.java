0e215d134e36c52c4d97234ebc2eaa4b22425cec
TerribleProgrammer124 <783827@pdsb.net>
2024-09-19 22:31:40 -0400

    Create L4Modify4.java

---
+++ L4Modify4.java
@@ -0,0 +1,10 @@
+class Main 
+{
+    public static void main(String[] args) 
+    {
+      int a = 8;
+      int b = 3;
+      System.out.println(a*b); // what is the output?24
+      System.out.println(a/b); // what is the output?2
+    }
+}
