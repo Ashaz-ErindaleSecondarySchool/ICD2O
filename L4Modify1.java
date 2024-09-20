a40d406e20e7d7eeea485e7879e6d7cb25566131
TerribleProgrammer124 <783827@pdsb.net>
2024-09-19 22:08:37 -0400

    Create L4Modify1.java

---
+++ L4Modify1.java
@@ -0,0 +1,9 @@
+class L4Modify1
+{
+    public static void main(String[] args) 
+    {
+      int amount = 24;
+      System.out.println(amount);
+      System.out.println("Two dozens is: " + amount);
+    }
+}
