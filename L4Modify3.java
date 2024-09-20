2cf2a39501c51d0be4154e83bf51cab1ed38b856
TerribleProgrammer124 <783827@pdsb.net>
2024-09-19 22:28:36 -0400

    Create L4Modify3.java

---
+++ L4Modify3.java
@@ -0,0 +1,10 @@
+class L4Modify3
+{
+    public static void main(String[] args) 
+    {
+      int amount = 8;
+      amount = amount + 1;
+      System.out.println(amount);
+      //amount has changed to the new incremented value after its initialization
+    }
+}
