4cc4ae2571d31f1f01b54aced0f4dd8c19e0c56f
TerribleProgrammer124 <783827@pdsb.net>
2024-09-19 22:38:19 -0400

    Create L4Last.java

---
+++ L4Last.java
@@ -0,0 +1,10 @@
+class L4Last
+{
+    public static void main(String[] args) 
+    {
+      double a = 8.0;
+      int b = 3;
+      System.out.println(a/b);
+      //It make the output a decimal
+    }
+}
