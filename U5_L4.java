147227d9da247daeb2e9b10f18f8ed9fb8d459c0
Ashaz Erindale <783827@pdsb.net>
2024-10-30 18:10:29 -0400

    Create U5_L4.java

---
+++ U5_L4.java
@@ -0,0 +1,14 @@
+class U5_L4
+{
+  public static void main(String[] args) 
+  {
+    // Make any needed corrections to the code below until it runs
+    // without errors.
+
+    String lastMonth = "August";
+
+    System.out.println("The last month was " + lastMonth);
+
+    //////////// End of tasks /////////////////////
+  }
+}
