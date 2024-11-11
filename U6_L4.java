ac33881fb5119e49c6dbbeeb06603bf977431c37
Ashaz Erindale <783827@pdsb.net>
2024-11-11 13:07:15 -0500

    Create U6_L4.java

---
+++ U6_L4.java
@@ -0,0 +1,13 @@
+class U6_L4
+{
+   public static void main(String [] args)
+   {
+      String w1 = "shellshock";
+      String w2 = "shellac";
+      String w3 = "fortify";
+      
+     System.out.println("The first word is: " + w3);
+     System.out.println("The second word is: " + w2);
+     System.out.println("The Third word is: " + w1);
+   }
+}
