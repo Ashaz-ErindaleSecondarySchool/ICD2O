96c0643c34d91582dcf9953da457ce1017f3e5dc
Ashaz Erindale <783827@pdsb.net>
2024-11-12 13:31:44 -0500

    Update U6_L4.java

--- U6_L4.java
+++ U6_L4.java
@@ -1,13 +1,22 @@
 class U6_L4
 {
-   public static void main(String [] args)
+   public static void main(String [] args) 
    {
       String w1 = "shellshock";
       String w2 = "shellac";
       String w3 = "fortify";
       
-     System.out.println("The first word is: " + w3);
-     System.out.println("The second word is: " + w2);
-     System.out.println("The Third word is: " + w1);
+      if (w3.compareTo(w1) < 0)
+     {
+        System.out.println("The first word is: "+w3);
+     }
+     if (w2.compareTo(w1) < 0)
+     {
+       System.out.println("The next word is: "+w2);
+     }
+     if (w1.compareTo(w3) > 0)
+     {
+       System.out.println("The last word is: "+w1);
+     }
    }
 }
