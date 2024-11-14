1749170d1986e1017cc5aa513725cb93c954c1ec
Ashaz Erindale <783827@pdsb.net>
2024-11-14 14:08:16 -0500

    Update U6_L4.java

--- U6_L4.java
+++ U6_L4.java
@@ -1,22 +1,38 @@
-class U6_L4
-{
-   public static void main(String [] args) 
-   {
+class U6_L4 {
+   public static void main(String [] args) {
       String w1 = "shellshock";
       String w2 = "shellac";
       String w3 = "fortify";
-      
-      if (w3.compareTo(w1) < 0)
-     {
-        System.out.println("The first word is: "+w3);
-     }
-     if (w2.compareTo(w1) < 0)
-     {
-       System.out.println("The next word is: "+w2);
-     }
-     if (w1.compareTo(w3) > 0)
-     {
-       System.out.println("The last word is: "+w1);
-     }
+     
+      if ((w1.compareTo(w2) < 0) && (w2.compareTo(w3) < 0) && (w3.compareTo(w1) > 0)) {
+        System.out.println(w1);
+        System.out.println(w2);
+        System.out.println(w3);
+      }
+      if ((w1.compareTo(w2) < 0) && (w2.compareTo(w3) > 0) && (w3.compareTo(w1) > 0)) {
+        System.out.println(w1);
+        System.out.println(w3);
+        System.out.println(w2);
+      }
+      if ((w1.compareTo(w2) > 0) && (w2.compareTo(w3) < 0) && (w3.compareTo(w1) > 0)) {
+        System.out.println(w2);
+        System.out.println(w1);
+        System.out.println(w3);
+      }
+      if ((w1.compareTo(w2) > 0) && (w2.compareTo(w3) < 0) && (w3.compareTo(w2) > 0)) {
+        System.out.println(w2);
+        System.out.println(w3);
+        System.out.println(w1);
+      }
+      if ((w1.compareTo(w2) > 0) && (w2.compareTo(w3) > 0) && (w3.compareTo(w1) < 0)) {
+        System.out.println(w3);
+        System.out.println(w2);
+        System.out.println(w1);
+      }
+      if ((w1.compareTo(w2) < 0) && (w2.compareTo(w3) > 0) && (w3.compareTo(w1) < 0)) {
+        System.out.println(w3);
+        System.out.println(w1);
+        System.out.println(w2);
+      }
    }
 }
