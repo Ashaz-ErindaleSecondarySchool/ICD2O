f32d37c05be9997b0aa8612e1b4caf59622a2bdd
Ashaz Erindale <783827@pdsb.net>
2024-11-20 22:31:48 -0500

    Create U6_L10.java

---
+++ U6_L10.java
@@ -0,0 +1,11 @@
+class U6_L10 
+{
+  public static void main(String [] args)
+  {
+    {
+        String s = "this set of brackets defines a scope";
+    }
+    System.out.println(s);
+    //It is the misuse of brackets, so an error will appear after running this program.
+  } 
+} 
